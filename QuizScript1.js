67028da1b10ab7154af4f26a273ce108276228f6
Melanie Smith <mels59@gmail.com>
2017-06-06 13:33:50 -0400

    leg strength

--- QuizScript1.js
+++ QuizScript1.js
@@ -225,3 +225,17 @@ $(document).ready(function () {
   });
 
 });
+
+
+$(document).ready(function () {
+  $('.carousel').carousel();
+});
+// Next slide
+$('.carousel').carousel('next');
+$('.carousel').carousel('next', 3); // Move next n times.
+// Previous slide
+$('.carousel').carousel('prev');
+$('.carousel').carousel('prev', 4); // Move prev n times.
+// Set to nth slide
+$('.carousel').carousel('set', 4);
+
