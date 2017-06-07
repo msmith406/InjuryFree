f2108f7542317978af5aa44b8e066a1778fb383e
Melanie Smith <mels59@gmail.com>
2017-06-07 14:04:14 -0400

    started disclaimer

--- QuizScript1.js
+++ QuizScript1.js
@@ -223,19 +223,6 @@ $(document).ready(function () {
   $('#submitButton').click(function () {
     $(this).addClass('hide');
   });
-
 });
 
-
-$(document).ready(function () {
-  $('.carousel').carousel();
-});
-// Next slide
-$('.carousel').carousel('next');
-$('.carousel').carousel('next', 3); // Move next n times.
-// Previous slide
-$('.carousel').carousel('prev');
-$('.carousel').carousel('prev', 4); // Move prev n times.
-// Set to nth slide
-$('.carousel').carousel('set', 4);
 
