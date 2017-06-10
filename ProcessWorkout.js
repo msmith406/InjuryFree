48ab104d452a38a9bb1036373d745e4515aeb177
Melanie Smith <mels59@gmail.com>
2017-06-10 15:23:01 -0400

    more work on the form

--- ProcessWorkout.js
+++ ProcessWorkout.js
@@ -12,9 +12,77 @@ function processWorkout()
   document.getElementById('numCardioDays').innerHTML = "Cardio Days: " + cardioDays;
   if (Number(liftingDays) + Number(cardioDays) > 6)
   {
-    document.getElementById('greaterThan6').innerHTML = "Either the user will not have an off day or cardio and lifting will" +
+    document.getElementById('greaterThan6').innerHTML = "Either your will not have an off day or cardio and lifting will" +
       " be on same day at least once."
   }
+  
+  
+  var weekDays=0;
+  var dayString="Days to workout: ";
+
+  var daysOfWeek = [document.getElementById('Sunday'), document.getElementById('Monday'), document.getElementById('Tuesday'), document.getElementById('Wednesday'), 
+  document.getElementById('Thursday'), document.getElementById('Friday'), document.getElementById('Saturday')];
+  var liftArray = [];
+   for(var i = 0, length = daysOfWeek.length; i < length; i++){
+	  var day = daysOfWeek[i];
+	  if(day.checked){
+		  dayString+=day.id+", ";
+		  weekDays++;
+		  liftArray.push(day.id);
+	  }
+   }
+  
+  var whichSections="";
+  var workLegs=document.getElementById('legs').checked;
+  var workUpper=document.getElementById('upperBody').checked;
+  var workTrunk=document.getElementById('trunk').checked;
+  var sectionArray = [workLegs, workUpper, workTrunk];
+  var numSections = sectionArray.length;
+  
+  if(weekDays==1){
+	whichSections+="DayOne: "+liftArray[0]+": ";
+	if(workLegs){
+		whichSections+="workLegs ";
+	}if(workUpper){
+		whichSections+="workUpper ";
+	}if(workTrunk){
+		whichSections+="workTrunk";
+	}
+  }
+  
+ document.getElementById('workoutPlan').innerHTML = whichSections;
+  
+  document.getElementById('daysToWorkout').innerHTML = dayString;
+  if(liftingDays<1){
+	 document.getElementById('validateNumDays').innerHTML = "Must have at least 1 lifting day";   
+  }
+  else if(weekDays < liftingDays || weekDays < cardioDays){
+	 document.getElementById('validateNumDays').innerHTML = "Not enough days selected."; 
+  }
+  else if(weekDays>(liftingDays+cardioDays)){
+	 document.getElementById('validateNumDays').innerHTML = "Too many days selected."; 
+  }
+  else{
+	document.getElementById('validateNumDays').innerHTML = "Valid number of days.";  
+  }
+  
+  getCardioLiftOrOff(daysOfWeek, weekDays, cardioDays, liftingDays);  
+}
+
+function getCardioLiftOrOff(dayArray, numWorkoutDays, cardioDays, liftDays){
+	var workoutPlan = "Workout Plan: ";
+	if(numWorkoutDays == liftDays){
+		for(var i = 0, length = dayArray.length; i < length; i++){
+			var day = dayArray[i];
+			workoutPlan+=day.id+": ";
+			if(day.checked){
+				workoutPlan+="lift, ";
+			}else{
+				workoutPlan+="off, ";
+			}
+		}
+	}
+	//document.getElementById('workoutPlan').innerHTML = workoutPlan; 
 }
 
 $(document).ready(function () {
